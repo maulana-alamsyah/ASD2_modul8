d87db23fa6375cbcf09986f6410909b1d969dbbc
maulana-alamsyah <maulanalamsyah@gmail.com>
2021-06-10 12:15:53 +0700

    Tugas 8: Binary Tree

---
+++ src/Tree/TreeNode.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tree;
+
+/**
+ *
+ * @author UserPCC
+ */
+public class TreeNode {
+    char data;
+    TreeNode left, right;
+    public TreeNode(char data){
+        this.data = data;
+    }
+
+    TreeNode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
